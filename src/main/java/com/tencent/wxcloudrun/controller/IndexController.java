cd8bd2a2e74c399b6e0a1c4598ef4c5415d10730
Catalina2014 <33884653+Catalina2014@users.noreply.github.com>
2024-03-31 18:41:06 +0800

    Update IndexController.java

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * index控制器
